7efd065ba79051bc7d497bfac1941d0af28b63c6
Divik Bhatia <divik.bhatia@gmail.com>
2024-04-22 19:26:04 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ export default async function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <h1 className={styles.title}></h1>
+        <h1 className={styles.title}>Get things done</h1>
+        <ul>
+          <li>Task 1</li>
+          <li>Task 2</li>
+          <li>Task 3</li>
+        </ul>
       </div>
     </main>
   );
